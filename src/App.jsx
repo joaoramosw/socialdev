d0cd1c144149ae083c00bd8d9f1023b038b08218
jvramoss <devjoaoramos@gmail.com>
2023-05-28 10:20:18 -0300

    :tada: Commit inicial

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { Header } from "./components/Header";
+import { Post } from "./post";
+
+export function App() {
+  return (
+  <div>
+    <Header/>
+    <Post author="João Ramos"
+      content="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Pariatur sunt provident quae velit quidem harum voluptates earum obcaecati quis! Nostrum magnam culpa sed cupiditate blanditiis et soluta rerum possimus exercitationem?
+      "
+      />
+    
+      
+  </div>  
+  )
+}
+export default App
---
+++ src/post.jsx
@@ -0,0 +1,6 @@
+export function Post(Props){
+    return (<div>
+        <strong>{Props.author}</strong>
+        <p>{Props.content}</p>
+    </div>)
+}
