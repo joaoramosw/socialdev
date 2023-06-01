be6c7196dd8a542222505d1b88a14edb58bedeab
jvramoss <devjoaoramos@gmail.com>
2023-05-31 23:55:48 -0300

    feat app + css post

--- src/App.jsx
+++ src/App.jsx
@@ -16,14 +16,6 @@ export function App() {
             author="João Victor"
             content="Um post muito divertido e Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores saepe omnis, quos illo perspiciatis ex magnam eos, ad doloribus laboriosam sed iure non quidem odit reprehenderit minus. Quibusdam, adipisci dolorum!"
           /> 
-          <Post 
-            author="Sérgio Ramos"
-            content="Um post muito divertido e Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores saepe omnis, quos illo perspiciatis ex magnam eos, ad doloribus laboriosam sed iure non quidem odit reprehenderit minus. Quibusdam, adipisci dolorum!"
-          />  
-          <Post 
-            author="Joseval Ramos"
-            content="Um post muito divertido e Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores saepe omnis, quos illo perspiciatis ex magnam eos, ad doloribus laboriosam sed iure non quidem odit reprehenderit minus. Quibusdam, adipisci dolorum!"
-          />  
          </main>
     </div>
   </div>  
