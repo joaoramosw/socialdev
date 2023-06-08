471b337fd833ce488c1608fe8490ad8b4a7bdee6
jvramoss <devjoaoramos@gmail.com>
2023-06-07 22:24:18 -0300

    fix app

--- src/App.jsx
+++ src/App.jsx
@@ -20,6 +20,10 @@ export function App() {
             author="João Victor"
             content="Um post muito divertido e Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores saepe omnis, quos illo perspiciatis ex magnam eos, ad doloribus laboriosam sed iure non quidem odit reprehenderit minus. Quibusdam, adipisci dolorum!"
           />  
+          <Post 
+            author="João Victor"
+            content="Um post muito divertido e Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores saepe omnis, quos illo perspiciatis ex magnam eos, ad doloribus laboriosam sed iure non quidem odit reprehenderit minus. Quibusdam, adipisci dolorum!"
+          />  
           
       
          </main>
