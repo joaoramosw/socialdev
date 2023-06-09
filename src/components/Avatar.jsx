6bbcc2a93262157838b5604944c7b053ad31f337
jvramoss <devjoaoramos@gmail.com>
2023-06-08 23:51:20 -0300

    fix avatar

--- src/components/Avatar.jsx
+++ src/components/Avatar.jsx
@@ -4,8 +4,5 @@ export function Avatar({}){
         <img className={props.hasBorder ? styles.avatarWithBorder : styles.avatar} 
         src={props.src}
         />
-        <img className={props.hasBorder ? styles.avatarWithBorder : styles.avatar} 
-        src={props.src}
-        />
     ) 
 }
