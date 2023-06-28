34d532f76c66fcd960256161556bcea1a0162575
jvramoss <devjoaoramos@gmail.com>
2023-06-28 01:22:31 -0300

    state like count

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,12 +1,21 @@
 import { HandsClapping, ThumbsUp, Trash } from 'phosphor-react'
 import styles from './Comment.module.css'
 import { Avatar } from './Avatar'
+import { useState } from 'react'
 
 export function Comment({content , onDeleteComment}){
-    
+    const [likeCount , setLikeCount] = useState(0)
+
+
     function handleDeleteComment(){
         onDeleteComment(content)
 
+    }
+
+    function handleLikeComment(){
+        setLikeCount((state)=>{
+            return state + 1
+        })
     }
 
     return(
@@ -30,9 +39,9 @@ export function Comment({content , onDeleteComment}){
                 </div>
                 
                 <footer>
-                    <button >
+                    <button onClick={handleLikeComment}>
                         <HandsClapping size={16}/>
-                        Aplaudir <span>14</span>
+                        Aplaudir <span>{likeCount}</span>
                     </button>
                 </footer>
             
